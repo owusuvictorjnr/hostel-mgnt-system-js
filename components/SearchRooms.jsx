8668d04d3976e50429074e2a3c59fc75b27a1eb4
owusuvictorjnr <owusuvictor80@gmail.com>
2024-03-29 17:15:10 +0000

    search room

---
+++ components/SearchRooms.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { CiSearch } from 'react-icons/ci'
+
+import { FaAngleDown } from 'react-icons/fa6'
+
+const SearchRooms = () => {
+  return (
+    <section className=" bg-secondary min-h-[10rem] container mx-auto rounded-lg pb-10 ">
+      <div className="grid grid-cols-1 md:grid-cols-3 pt-10 px-5 capitalize font-bold md:text-xl md:space-x-5">
+        <div className="">
+          <h1 className="">room type</h1>
+          <div className="flex">
+            <input type="text" className="py-1 px-2  rounded-lg w-full" />
+            <FaAngleDown className="mt-2" />
+          </div>
+        </div>
+
+        <div className="">
+          <h1 className="">search</h1>
+
+          <div className="flex">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="py-1 px-2 rounded-lg w-full"
+            />
+            <CiSearch className="mt-2" />
+          </div>
+        </div>
+
+        <button className=" bg-primary px-5 py-1 mt-4 rounded-lg capitalize text-white lg:w-1/2">
+          search
+        </button>
+      </div>
+    </section>
+  )
+}
+
+export default SearchRooms
